7b9f1cebfca5e262938deeb594d3feaf7520e4f6
victor <luanvitorlima2017@gmail.com>
2025-08-03 21:00:49 -0300

    estilizando meu cubo

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 var canvas = document.getElementById('interface');
 var context = canvas.getContext('2d');
 
+context.fillStyle = 'red';
 context.fillRect(50, 50, 100, 100);
+
+context.lineWidth = 3;
+context.strokeStyle = 'blue';
+context.strokeRect(50, 50, 100, 100)
